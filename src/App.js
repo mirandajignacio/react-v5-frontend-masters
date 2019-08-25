f336b89640c7b86f103ccdaec6d8c4d0df25f209
Miranda J Ignacio <mirandajignacio@gmail.com>
2019-08-25 13:49:24 -0300

    🐕

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
 import SearchParams from './SearchParams';
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt me!</h1>
-      <SearchParams></SearchParams>
-      {/* <Pet name="Croket" animal="Dog" color="Black & white"></Pet>
-      <Pet name="Lila" animal="Dog" color="Blond"></Pet>
-      <Pet name="Romeo" animal="Dog" color="Blond"></Pet> */}
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1>Adopt me!</h1>
+        <SearchParams></SearchParams>
+      </div>
+    </React.StrictMode>
   );
 };
 
